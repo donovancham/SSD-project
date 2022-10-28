ab4a097f9a1b73882737c34154f06a02edd2b5d2
Kenrick78 <kenong7878@gmail.com>
2022-10-28 13:29:15 +0800

    Registration page with bric and user role

--- 3203ssd/apps/authentication/models.py
+++ 3203ssd/apps/authentication/models.py
@@ -14,6 +14,8 @@ class Users(db.Model, UserMixin):
     username = db.Column(db.String(64), unique=True)
     email = db.Column(db.String(64), unique=True)
     password = db.Column(db.LargeBinary)
+    userrole=db.Column(db.String(64))
+    nric=db.Column(db.String(64), unique=True)
 
     def __init__(self, **kwargs):
         for property, value in kwargs.items():
